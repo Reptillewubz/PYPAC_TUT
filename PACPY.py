93de4143385821e451c6532f1985a550d6bb2513
Reptillewubz <58353191+Reptillewubz@users.noreply.github.com>
2019-11-29 21:38:27 -0800

    Add files via upload

---
+++ PACPY.py
@@ -0,0 +1,10 @@
+from app_class import *
+from settings import *
+from PACPY import *
+import turtle
+import os
+if __name__ == "__PACPY__":
+
+    app = App()
+    app.run()
+#answer = input('wrong')
---
+++ settings.py
@@ -0,0 +1,4 @@
+from app_class import *
+from PACPY import *
+WIDTH, HEIGHT = 800, 800 
+FPS = 60
